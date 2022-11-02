abfa03066547ad771b002dd6ccc9a8564e254169
Nicolas PELZMANN <nicoals.pelzmann@gmail.com>
2022-11-02 12:18:21 +0100

    centralized solve for simple network

--- main.py
+++ main.py
@@ -2,49 +2,43 @@ from cvxpy import *
 import numpy as np
 import scipy as sp
 from scipy import sparse
+import matplotlib.pyplot as plt
 
 import utils
 
 nw = utils.network()
 
-# Discrete time model of a quadcopter
-Ad = nw.A
-Bd = nw.B
-nx = nw.nx
-nu = nw.nu
-
-# Constraints
-u0 = np.zeros(nu)
-
-# Objective function
-Q = nw.Q
-P = nw.P
-R = nw.R
-
-# Initial and reference states
-x0 = np.zeros(nx)
-xr = np.zeros(nx)
-xr[0:2:-1] = 1
-
 # Prediction horizon
-N = 10
+N = 5
 
 # Define problem
-u = Variable((nu, N))
-x = Variable((nx, N+1))
-x_init = Parameter(nx)
+u = Variable((nw.nu, N))
+x = Variable((nw.nx, N+1))
+x_init = Parameter(nw.nx)
 objective = 0
 constraints = [x[:,0] == x_init]
 for k in range(N):
-    objective += quad_form(x[:,k] - xr, Q) + quad_form(u[:,k], R)
-    constraints += [x[:, k+1] == Ad*x[:, k] + Bd*u[:, k]]
+    objective += quad_form(x[:,k] - nw.xt, nw.Q) + quad_form(u[:,k], nw.R)
+    constraints += [x[:, k+1] == nw.A @ x[:, k] + nw.B @ u[:, k]]
     constraints += [nw.E_x @ x[:, k] + nw.E_u @ u[:, k] <= nw.b]
-objective += quad_form(x[:,N] - xr, P)
+objective += quad_form(x[:,N] - nw.xt, nw.P)
 prob = Problem(Minimize(objective), constraints)
 
 # Simulate in closed loop
+x0 = nw.x0
 nsim = 15
+trajectory = np.zeros((nw.nx, nsim + 1))
+trajectory[:, 0] = x0
+applied_u = np.zeros((nw.nu, nsim))
 for i in range(nsim):
     x_init.value = x0
     prob.solve(solver=OSQP, warm_start=True)
-    x0 = Ad.dot(x0) + Bd.dot(u[:,0].value)
+    x0 = nw.A.dot(x0) + nw.B.dot(u[:,0].value)
+    trajectory[:, i+1] = x0
+    applied_u[:, i] = u[:,0].value
+
+for i in range(nw.M):
+    plt.plot(trajectory[4*i, :], trajectory[4*i+2, :], marker='x')
+
+plt.show()
+print('tst')
--- utils.py
+++ utils.py
@@ -6,13 +6,18 @@ from numpy import eye
 
 class node(object):
 
-    def __init__(self) -> None:
-        self.A = sp.kron(eye(2), eye(2,2,1)).todense()
-        self.B = sp.kron(eye(2), np.array([[1.], [0.]])).todense()
+    def __init__(self, 
+                 x0: list = [0., 0., 0., 0.],
+                 xt: list = [0., 0., 0., 0.]) -> None:
+        
+        self.x0 = x0
+        self.xt = xt
+        self.A = sp.kron(eye(2), np.array([[1., 1.], [0., 1.]])).todense()
+        self.B = sp.kron(eye(2), np.array([[0.], [1.]])).todense()
         
         self.nx, self.nu = np.shape(self.B)
 
-        self.R = eye(self.nu)
+        self.R = eye(self.nu) * 5
         self.Q = eye(self.nx)
         self.P = la.solve_discrete_are(self.A, self.B, self.Q, self.R)
 
@@ -20,18 +25,25 @@ class node(object):
 class network(object):
 
     def __init__(self) -> None:
-        self.nodes = np.array([node(), node(), node()])
+        self.nodes = np.array([node(x0=[0., 0.5, 0., -0.5], xt=[1., 0.,  1.,  0.]), 
+                               node(x0=[1.5, -0.5, 0., 0.], xt=[2.,   0.,  1.,  0.]), 
+                               node(x0=[0.5, 0.2, 0.7, -0.2], xt=[1.5, 0., 1 + 1/np.sqrt(2), 0.])])
         self.D = np.array([[1, 0, 1], [-1, 1, 0], [0, -1, -1]])
         
-        nu = A = np.sum(*list(n.nu for n in self.nodes))
-        nx = A = np.sum(*list(n.nx for n in self.nodes))
-
-        A = la.block_diag(*list(n.A for n in self.nodes))
-        B = la.block_diag(*list(n.B for n in self.nodes))
-        R = la.block_diag(*list(n.R for n in self.nodes))
+        self.M = len(self.nodes)
+        self.nu = np.sum([n.nu for n in self.nodes])
+        self.nx = np.sum([n.nx for n in self.nodes])
+        
+        self.x0 = np.hstack([n.x0 for n in self.nodes])
+        self.xt = np.hstack([n.xt for n in self.nodes])
+        self.A = la.block_diag(*[n.A for n in self.nodes])
+        self.B = la.block_diag(*[n.B for n in self.nodes])
+        self.R = la.block_diag(*[n.R for n in self.nodes])
+        self.Q = la.block_diag(*[n.Q for n in self.nodes])
+        self.P = la.block_diag(*[n.P for n in self.nodes])
     
         sel_mat = np.array([[1, 0, 0, 0], [0, 0, 1, 0]])
-        self.E_x= la.kron(self.D, sel_mat)
+        self.E_x= la.kron(self.D.T, sel_mat)
         self.E_u = np.zeros((np.shape(self.E_x)[0], self.nu))
         self.b = np.ones(np.shape(self.E_x)[0])
     
