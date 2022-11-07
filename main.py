f4dd7cf19eb645042eaf14f4b62841a2a97aa680
Nicolas PELZMANN <nicoals.pelzmann@gmail.com>
2022-11-07 19:20:11 +0100

    working example

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 import utils
 
-nw = utils.network(N=5)
+nw = utils.network(N=8, ada_eps=1e-3, ada_iterations=300, ada_alpha=0.9)
 
 # Simulate in closed loop
 nsim = 15
@@ -12,11 +12,13 @@ trajectory[:, 0] = nw.x
 control_inputs = np.zeros((nw.nu, nsim))
 for i in range(nsim):
     # u_centr = nw.centr_solve()
-    u_centr2 = nw.centr_solve2()
-    u = nw.ada_solve()
+    u_centr2, lda_centr2 = nw.centr_solve2()
+    u, lda = nw.ada_solve()
     nw.simulate_timestep(u)
     trajectory[:, i+1] = nw.x
     control_inputs[:, i] = np.hstack(u)
+    if i == 0:
+        nw.plot_convergence()
 
 for i in range(nw.M):
     plt.plot(trajectory[4*i, :], trajectory[4*i+2, :], marker='x')
--- utils.py
+++ utils.py
@@ -130,12 +130,12 @@ class network(object):
 
     def __init__(self, 
                  N: int = 2, 
-                 ada_iterations: int = 20,
+                 ada_iterations: int = 100,
                  ada_eps: float = 1e-4,
-                 ada_alpha: float = 0.5) -> None:
-        self.nodes = np.array([node(x0=[0., 0.5, 0., -0.5], xt=[1., 0.,  1.,  0.], N=N), 
-                               node(x0=[1.5, -0.5, 0., 0.], xt=[2.,   0.,  1.,  0.], N=N), 
-                               node(x0=[0.5, 0.2, 0.7, -0.2], xt=[1.5, 0., 1 + 1/np.sqrt(2), 0.], N=N)])
+                 ada_alpha: float = 0.9) -> None:
+        self.nodes = np.array([node(x0=[1.5, -0.3, 0., 0.], xt=[1.,   0.,  1.,  0.], N=N),
+                               node(x0=[0.5, -0.3, 0., -0.4], xt=[2., 0.,  1.,  0.], N=N),  
+                               node(x0=[0.5,  0.2, 0.7, -0.2], xt=[1.5, 0., 2., 0.], N=N)])
         self.D = np.array([[1, 0, 1], [-1, 1, 0], [0, -1, -1]])
 
         self.N = N
@@ -153,7 +153,7 @@ class network(object):
         self.R = la.block_diag(*[n.R for n in self.nodes])
         self.Q = la.block_diag(*[n.Q for n in self.nodes])
         self.P = la.block_diag(*[n.P for n in self.nodes])
-        sel_mat = np.array([[1, 0, 0, 0], [0, 0, 1, 0]])
+        sel_mat = np.array([[1, 0, 0, 0], [-1, 0, 0, 0], [0, 0, 1, 0], [0, 0, -1, 0]])
         self.E_x = la.kron(self.D.T, sel_mat)
         self.E_u = np.zeros((np.shape(self.E_x)[0], self.nu))
         self.C_u = la.block_diag(*[n.C_u for n in self.nodes])
@@ -162,7 +162,7 @@ class network(object):
 
         self.x_init = cvxpy.Parameter(self.nx)
         self.u_var = cvxpy.Variable((self.nu, N))
-        self.lda = None
+        self.lda_ada = self.lda_centr = None
 
         self.dense_agent_problem_matrices()
         
@@ -198,10 +198,10 @@ class network(object):
         for n in self.nodes:
             u.append(n.z[0:n.nu].value)
         self.lda_centr = self.centr_prob2.constraints[-1].dual_value
-        return u
+        return u, self.lda_centr
 
     def ada_solve(self) -> List[np.array]:
-        lda0 = np.zeros(self.N * self.E_x.shape[0]) if self.lda is None else self.lda
+        lda0 = np.zeros(self.N * self.E_x.shape[0]) if self.lda_ada is None else self.lda_ada[:, -1]
         lda = np.zeros((lda0.shape[0], self.lb + 1))
         lda[:, 0] = lda0
         mu = lda0
@@ -220,9 +220,9 @@ class network(object):
             theta = theta_new
             mu = mu_new
         
-        self.lda = lda[:, -1]
+        self.lda_ada = lda
 
-        return u
+        return u, lda
 
     def centr_prob_def(self) -> None:
         x = cvxpy.Variable((self.nx, self.N+1))
@@ -253,10 +253,11 @@ class network(object):
         self.centr_prob2 = cvxpy.Problem(cvxpy.Minimize(objective), constraints)
         
     def plot_convergence(self) -> None:
-        lda_err = self.lda - self.lda_centr
+        lda_err = (self.lda_ada.T - self.lda_centr).T
         lda_err_norm = np.linalg.norm(lda_err, ord=1, axis=0)
         plt.plot(lda_err_norm)
-        plt.xlabel('Iterations l')
-        plt.ylabel('$||\lambda_l - \lambda_\text{centr}||_1$')
+        plt.yscale('log')
+        plt.xlabel('Iterations $l$')
+        plt.ylabel(r'$||\lambda_{\epsilon, l} - \lambda_{centr}||_1$')
         plt.show()
             
